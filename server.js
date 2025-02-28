df5ef3ec451583d4d15b9782a2f2bd2205d5ad1c
Lens Kamdem <kamdem.lens@gmail.com>
2025-02-28 23:26:23 +0000

    Added hash and compare hash functions with Bcrypt

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -10,6 +11,18 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const bcrypt = require("bcrypt");
+
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
+
+
 
 
 
@@ -17,7 +30,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_SYNC
 
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+  console.log(hash);
+  let result = bcrypt.compareSync(myPlaintextPassword, hash);
+  console.log(result);
 
 //END_SYNC
 
